2a29fe0999ffb82a3a980a7f36f975e33c82f479
Augustin Diabira <augustindiabira@icloud.com>
2022-08-09 17:59:44 +0200

    Initial Commit

---
+++ SpotifyClone/SpotifyCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyCloneApp.swift
+//  SpotifyClone
+//
+//  Created by Augustin Diabira on 09/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
