e6aa4862a1a35c36bcd344ebcebba6276ebd2ece
Augustin Diabira <augustindiabira@icloud.com>
2022-08-09 23:18:54 +0200

    Try testing

--- SpotifyClone/ContentView.swift
+++ SpotifyClone/ContentView.swift
@@ -9,8 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                
+                
+                Image("spotify").resizable().frame(width: 180, height: 80).padding()
+             
+                
+                VStack {
+                    Text("We Play the music.").font(.system(size: 32)).fontWeight(.heavy)
+                    Text("You enjoy it. Deal ?").font(.system(size: 32)).fontWeight(.heavy)
+                }.padding(.bottom, 44)
+              
+                VStack {
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("SIGN UP").frame(width: 260).font(.headline)
+                    }.padding().foregroundColor(.white)  .background(Color.green)
+                        .background(.ultraThickMaterial, in: RoundedRectangle( cornerRadius: 25)).frame(width: 300)
+
+                    
+                      
+                        
+                        NavigationLink {
+                            SpotifyLogPage()
+                        } label: {
+                            Text("LOG IN").frame(width: 260).padding().foregroundColor(.black) .font(.headline) .background(Color.white)
+                                .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 25)).overlay(Capsule().stroke(Color.black)).frame(width: 300).padding()
+                        }
+
+
+                    
+
+                    
+                    
+                }.padding(.bottom, 111)
+                
+                Text("By Clicking on sign up. You agree to Spotify's Terms and Conditions of use").padding().font(.subheadline)
+                
+            }
+        }
     }
 }
 
---
+++ SpotifyClone/SpotifyLogPage.swift
@@ -0,0 +1,47 @@
+//
+//  SpotifyLogPage.swift
+//  SpotifyClone
+//
+//  Created by Augustin Diabira on 09/08/2022.
+//
+
+import SwiftUI
+
+struct SpotifyLogPage: View {
+    @State var name:String = ""
+    @State var password:String = ""
+    var body: some View {
+        VStack {
+            Image("spotify").resizable().frame(width: 180, height: 80).padding()
+         
+            TextField("Email address or username", text: $name).frame(width: 260).padding().foregroundColor(.black) .font(.headline) .background(Color.white)
+                .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 25)).overlay(Capsule().stroke(Color.black)).frame(width: 300)
+            TextField("Password", text: $name).frame(width: 260).padding().foregroundColor(.black) .font(.headline) .background(Color.white)
+                .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 25)).overlay(Capsule().stroke(Color.black)).frame(width: 300).padding()
+            
+            Button {
+                
+            } label: {
+                Text("LOG IN").frame(width: 260).padding().foregroundColor(.white) .font(.headline) .background(Color.green)
+                    .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 25)).frame(width: 300).padding()
+
+            }
+            
+            Text("or continue with")
+            
+            HStack {
+                Image("facebookf").resizable().frame(width: 65, height: 65)
+                Image("apple").resizable().frame(width: 65, height: 65)
+            }
+            
+            Text("Forgot your password ?").font(.headline).padding(.top)
+
+        }.navigationBarBackButtonHidden(true)
+    }
+}
+
+struct SpotifyLogPage_Previews: PreviewProvider {
+    static var previews: some View {
+        SpotifyLogPage()
+    }
+}
